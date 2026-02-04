8d959a27ba40149b33d04e854afe68fd22d942e9
Jasmine Aqilah Rahmadia <rahmadiajasmineer@gmail.com>
2026-02-04 20:24:55 +0700

    feat: add show balance

--- routes/wallet.js
+++ routes/wallet.js
@@ -4,7 +4,7 @@ app.use(express.json())
 let walletController = require("../controllers/walletController")
 const authorization = require("../middlewares/authorization")
 
-app.get("/", [authorization.authorization], walletController.showBalance)
+app.post("/balance", [authorization.authorization], walletController.showBalance)
 app.post("/topup", [authorization.authorization], walletController.topUp)
 app.post("/payment", [authorization.authorization], walletController.payment)
 
